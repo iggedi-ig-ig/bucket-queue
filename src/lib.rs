01d86bdd76d39e0b8a25850b2c23da6ec8d59846
igs <iggedi@protonmail.com>
2023-11-10 12:20:10 +0100

    implement decrease_key

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,84 @@
-use num_traits::{Num, NumCast};
+//! # bucket-queue
+//! A bucket queue implementation in the rust programming language.
+//!
+//! # Bucket Queues
+//! Bucket queues are a specialized priority queue data structure that work well for monotonous, integer data with some maximum increment.
+//! Their most common application are for Dijkstra's algorithm for shortest paths, but they can be applied to any problem where:
+//! - keys are integers
+//! - keys are monotonous, meaning after the smallest key was extracted, no key that's smaller than the previous minimum can be added
+//! - there is a constant (small) range of values that can be added to the queue
+//!
+//! This is true for for example Dijkstra on Graphs with integer edge weights with a maximum edge weight of C, because:
+//! - edge weights are integers
+//! - if a value is pushed to the queue, it will have the value of the previous minimum plus some edge weight
+//! - that edge weight will be smaller than or equal to C
+//! This is also the most common application for bucket queues.
+//!
+//! # Time Complexity
+//! For a maximum increment of C, the operations have the following (worst case) runtime complexities:
+//! Operation | Complexity
+//! ---|---
+//! `insert` | O(1)
+//! `remove` | O(1)
+//! `decrease_key` | O(1)
+//! `get_min` | O(C)
+//! `pop_min` | O(C)
+//!
+//! Also note that these time complexities are all "real" upper bounds for worst-case time complexity, not amortized.
+//! This is better than anything a general purpose priority queue can achieve.
+//! Using a bucket queue, Dijkstra's algorithm can be implemented in O(|E| + |V|C) time.
+#![deny(missing_docs)]
+
+use std::marker::PhantomData;
 
 /// A monotone priority queue with all common operations being O(C) where C is a constant.
 /// This data structure is comonly used to implement Dijkstra's algorithm for shortest paths
 /// on graphs with small, non-negatie, integer edge weights with maximum weight C which,
-/// using this data strucutre, has run time complexity of O(n + mC)
-/// (instead of O(n + m log n) using fibonacci heaps.)
+/// using this data strucutre, has run time complexity of O(m + nC)
+/// (instead of O(m + n log n) using fibonacci heaps.)
 ///
 /// The idea of this data structure is based on the fact that in dijkstra, when scanning any
-/// given node, the maximum value that can be added to the queue is m + C,
-/// where m is the distance of the previously scanned node and C is the maximum edge weight,
+/// given node, the maximum value that can be added to the queue is M + C,
+/// where M is the distance of the previously scanned node and C is the maximum edge weight,
 /// and the next scanned node will have at least a distance of m to the origin.
 ///
+/// Internally, this data structure converts all `T`s to `usize`, so it might act unexpectedly
+/// when `T` holds state or something similar.
+/// This data structure is meant to only be used with `T` being a primitive.
+///
 /// # Operations
-/// ```text
-/// + ------------ + ----- +
-/// | insert       |  O(1  |
-/// + ------------ + ----- +
-/// | remove       |  O(1) |
-/// + ------------ + ----- +
-/// | pop_min      |  O(C) |
-/// + ------------ + ----- +
-/// | get_min      |  O(C) |
-/// + ------------ + ----- +
-/// | decrease_key |  O(1) |
-/// + ------------ + ----- +
-/// ```
+/// Operation | Complexity
+/// ---|---
+/// `insert` | O(1)
+/// `remove` | O(1)
+/// `decrease_key` | O(1)
+/// `get_min` | O(C)
+/// `pop_min` | O(C)
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct BucketQueue<T>
 where
-    T: Copy + Num + NumCast + PartialOrd,
+    usize: From<T>,
+    T: From<usize>,
 {
     buckets: Vec<usize>,
-    max_increment: T,
-    last_min: T,
+    max_increment: usize,
+    last_min: usize,
+    __phantom: PhantomData<T>,
 }
 
 impl<T> BucketQueue<T>
 where
-    T: Copy + Num + NumCast + PartialOrd,
+    usize: From<T>,
+    T: From<usize>,
 {
     /// Create a new [`BucketQueue`], given the maximum increment of values.
     pub fn new(max_increment: T) -> Self {
+        let max_increment = max_increment.into();
         Self {
-            buckets: vec![0; (max_increment + T::one()).to_usize().unwrap()],
+            buckets: vec![0; max_increment + 1],
             max_increment,
-            last_min: T::zero(),
+            last_min: 0,
+            __phantom: PhantomData::default(),
         }
     }
 
@@ -53,14 +88,58 @@ where
     /// This method panics if the parameter is not at least `last_min` (the value most recently returned
     /// by `pofp_min`) or not smaller than or equal to `last_min` + `max_increment` (`max_increment` was specified at creation)
     pub fn insert(&mut self, value: T) {
+        let value: usize = value.into();
         assert!(
             value < self.last_min + self.max_increment && value >= self.last_min,
             "BucketQueues only work if inserted elements are monotone, and only increase by a maximum of `max_step`"
         );
 
-        self.buckets[(value % (self.max_increment + T::one()))
-            .to_usize()
-            .unwrap()] += 1;
+        let index = value % (self.max_increment + 1);
+        self.buckets[index] += 1;
+    }
+
+    /// Removes an element if it is present. If it was present, it is returned.
+    pub fn remove(&mut self, value: T) -> Option<T> {
+        let value: usize = value.into();
+        assert!(
+            value < self.last_min + self.max_increment && value >= self.last_min,
+            "Value has to be between `last_min` and `last_min + max_step`"
+        );
+
+        let index = value % (self.max_increment + 1);
+        if let Some(new_value) = self.buckets[index].checked_sub(1) {
+            self.buckets[index] = new_value;
+            Some(value.into())
+        } else {
+            None
+        }
+    }
+
+    /// Moves an element from priority `value` to `new_key`. If no element with `value` exists, nothing changes.
+    /// Note that `value` and `new_key` both have to be between `last_min` and `last_min + max_increment`, 
+    /// and `new_key` has to be smaller than or equal to `value`. Otherwise, this method panics.
+    pub fn decrease_key(&mut self, value: T, new_key: T) {
+        let value: usize = value.into();
+        assert!(
+            value < self.last_min + self.max_increment && value >= self.last_min,
+            "Value has to be between `last_min` and `last_min + max_step`"
+        );
+        let old_index = value % (self.max_increment + 1);
+
+        let new_key: usize = new_key.into();
+        assert!(
+            value < self.last_min + self.max_increment && value >= self.last_min,
+            "New key has to be between `last_min` and `last_min + max_step`"
+        );
+        let new_index = new_key % (self.max_increment + 1);
+
+        // technically, the key could just aswell increase as long as it doesn't get larger than last_min + max_increment
+        assert!(new_key <= value, "Key has to decrease");
+
+        if let Some(new_count) = self.buckets[old_index].checked_sub(1) {
+            self.buckets[old_index] = new_count;
+            self.buckets[new_index] += 1;
+        }
     }
 
     /// Returns the smallest element from this queue, without removing it. To return and remove the smallest
@@ -72,12 +151,9 @@ where
     /// (`max_increment` was specified at creation).
     /// This method returns `None` if the queue is empty.
     pub fn get_min(&self) -> Option<T> {
-        (0..=self.max_increment.to_usize().unwrap())
-            .map(|offset| self.last_min.to_usize().unwrap() + offset)
-            .find(|&index| {
-                self.buckets[index % (self.max_increment + T::one()).to_usize().unwrap()] > 0
-            })
-            .map(|index| T::from(index).unwrap())
+        (self.last_min..=self.last_min + self.max_increment)
+            .find(|&index| self.buckets[index % (self.max_increment + 1)] > 0)
+            .map(|index| T::from(index))
     }
 
     /// Returns the smallest element from this queue and removes it. To only read the current minimum, call
@@ -90,19 +166,15 @@ where
     /// This method returns `None` if the queue is empty.
     ///
     /// # Side effects
-    /// Internaly, this method updates the `last_min` field, meaning that after this method is called, only
-    /// elements larger than or equal to its return value can be added to the queue. (see `insert`)
+    /// Internaly, this method updates `last_min`, meaning that after this method is called, only
+    /// elements larger than or equal to its return value can still be added to the queue. (see `insert`)
     pub fn pop_min(&mut self) -> Option<T> {
-        if let Some(min_index) = (0..=self.max_increment.to_usize().unwrap())
-            .map(|offset| self.last_min.to_usize().unwrap() + offset)
-            .find(|&index| {
-                self.buckets[index % (self.max_increment + T::one()).to_usize().unwrap()] > 0
-            })
+        if let Some(min_index) = (self.last_min..=self.last_min + self.max_increment)
+            .find(|&index| self.buckets[index % (self.max_increment + 1)] > 0)
         {
-            let min = T::from(min_index).unwrap();
-            self.last_min = min;
-            self.buckets[min_index % (self.max_increment + T::one()).to_usize().unwrap()] -= 1;
-            Some(min)
+            self.last_min = min_index;
+            self.buckets[min_index % (self.max_increment + 1)] -= 1;
+            Some(min_index.into())
         } else {
             None
         }
@@ -127,6 +199,23 @@ mod tests {
         assert_eq!(queue.pop_min(), Some(5));
         assert_eq!(queue.pop_min(), Some(7));
         assert_eq!(queue.pop_min(), None);
+    }
+
+    #[test]
+    fn remove() {
+        let mut queue = BucketQueue::new(10);
+
+        queue.insert(5);
+        queue.insert(4);
+        queue.insert(6);
+        queue.insert(3);
+
+        assert_eq!(queue.remove(3), Some(3));
+        assert_eq!(queue.remove(4), Some(4));
+
+        assert_eq!(queue.remove(3), None);
+
+        assert_eq!(queue.pop_min(), Some(5));
     }
 
     #[test]
